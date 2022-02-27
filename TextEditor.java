422a094e53bc7e2ead9a380f7896fd8523f13375
dtang1999 <84108333+dtang1999@users.noreply.github.com>
2022-02-27 02:07:01 -0800

    Add files via upload

---
+++ TextEditor.java
@@ -0,0 +1,435 @@
+package labs.lab9;
+
+
+
+import java.awt.BorderLayout;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Stack;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+
+/**
+ * This frame contains a text sample and a control panel to change the font of
+ * the text.
+ */
+public class TextEditor extends JFrame {
+	private static final int FRAME_WIDTH = 600;
+	private static final int FRAME_HEIGHT = 500;
+
+	private JCheckBox italicCheckBox;
+	private JCheckBox boldCheckBox;
+	
+//	private JRadioButton smallButton;
+//	private JRadioButton mediumButton;
+//	private JRadioButton largeButton;
+	
+	private JRadioButton size8Button;
+	private JRadioButton size16Button;
+	private JRadioButton size24Button;
+	private JRadioButton size32Button;
+	private JRadioButton size40Button;
+	
+	private JComboBox<String> facenameCombo;
+	private ActionListener listener;
+	private JTextArea textArea;
+	private JMenuBar menuBar;
+	private JButton clearButton;
+	private Stack<Font> textStack;
+	private Stack<Font> redoStack;
+	
+//	private Stack<doClass> textStack;
+//	private Stack<doClass> redoStack;
+	
+
+	/**
+	 * Constructs the frame.
+	 */
+	public TextEditor() {
+		// Construct text sample
+//		label = new JLabel("Big Java");
+//		add(label, BorderLayout.CENTER);
+		textStack = new Stack<>();
+		redoStack = new Stack<>();
+		setTitle("Zhesen Tang - 72984390");
+		textArea();
+		createClearButton();
+		menus();
+
+		// This listener is shared among all components
+		listener = new ChoiceListener();
+
+		createControlPanel();
+		setLabelFont();
+		setSize(FRAME_WIDTH, FRAME_HEIGHT);
+		
+	}
+
+	class ChoiceListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			setLabelFont();
+		}
+	}
+	
+	class ExitItemListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.exit(0);
+		}
+	}
+	
+	class clearListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			textArea.setText("");
+		}
+	}
+	
+	class undoListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			try {
+				undoEvent();
+			} catch (Exception ex) {
+				System.out.println("No step to undo!");
+			}
+//			undoEvent();
+			
+		}
+	}
+	
+	class redoListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			try {
+				redoEvent();
+			} catch (Exception ex) {
+				System.out.println("No step to redo!");
+			}
+			
+		}
+	}
+	
+	//text field
+	public void textArea() {
+		textArea = new JTextArea();
+		textArea.setText("");
+		textArea.setLineWrap(true);
+		textArea.setEditable(true);
+		
+		JPanel textPanel = new JPanel();
+		textPanel.setLayout(new GridLayout());
+		JScrollPane scroll = new JScrollPane(textArea);
+		textPanel.add(scroll);
+		
+		add(textPanel, BorderLayout.CENTER);
+	}
+	
+	public void createClearButton() {
+		clearButton = new JButton("Clear");
+		ActionListener listener = new clearListener();
+		clearButton.addActionListener(listener);
+		add(clearButton, BorderLayout.SOUTH);
+	}
+	
+	public void menus() {
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		menuBar.add(createFileMenu());
+		menuBar.add(createEditMenu());
+		
+	}
+	
+	public JMenu createFileMenu() {
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem exit = new JMenuItem("Exit");
+		ActionListener exitListener = new ExitItemListener();
+		exit.addActionListener(exitListener);
+		fileMenu.add(exit);
+		return fileMenu;
+	}
+	
+	public JMenu createEditMenu() {
+		JMenu editMenu = new JMenu("Edit");
+		
+		JMenuItem undoItem = new JMenuItem("Undo");
+		ActionListener undo = new undoListener();
+		undoItem.addActionListener(undo);
+		editMenu.add(undoItem);
+		
+		JMenuItem redoItem = new JMenuItem("Redo");
+		ActionListener redo = new redoListener();
+		redoItem.addActionListener(redo);
+		editMenu.add(redoItem);
+		
+		
+		return editMenu;
+		
+	}
+
+	/**
+	 * Creates the control panel to change the font.
+	 */
+	public void createControlPanel() {
+		JPanel facenamePanel = createComboBox();
+		JPanel styleGroupPanel = createCheckBoxes();
+		JPanel sizeGroupPanel = createRadioButtons();
+
+		// Line up component panels
+
+		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout(new GridLayout(3, 1));
+		controlPanel.add(facenamePanel);
+		controlPanel.add(styleGroupPanel);
+		controlPanel.add(sizeGroupPanel);
+
+		// Add panels to content pane
+
+		add(controlPanel, BorderLayout.NORTH);
+	}
+
+	/**
+	 * Creates the combo box with the font style choices.
+	 * 
+	 * @return the panel containing the combo box
+	 */
+	public JPanel createComboBox() {
+		facenameCombo = new JComboBox<String>();
+		facenameCombo.addItem("Serif");
+		facenameCombo.addItem("SansSerif");
+		facenameCombo.addItem("Monospaced");
+		facenameCombo.setEditable(true);
+		facenameCombo.addActionListener(listener);
+		
+
+		JPanel panel = new JPanel();
+		panel.add(facenameCombo);
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Font"));
+		return panel;
+	}
+
+	/**
+	 * Creates the check boxes for selecting bold and italic styles.
+	 * 
+	 * @return the panel containing the check boxes
+	 */
+	public JPanel createCheckBoxes() {
+		italicCheckBox = new JCheckBox("Italic");
+		italicCheckBox.addActionListener(listener);
+
+		boldCheckBox = new JCheckBox("Bold");
+		boldCheckBox.addActionListener(listener);
+
+		JPanel panel = new JPanel();
+		panel.add(italicCheckBox);
+		panel.add(boldCheckBox);
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Style"));
+
+		return panel;
+	}
+
+	/**
+	 * Creates the radio buttons to select the font size.
+	 * 
+	 * @return the panel containing the radio buttons
+	 */
+	public JPanel createRadioButtons() {
+		size8Button = new JRadioButton("8 pt.");
+		size8Button.addActionListener(listener);
+		
+		size16Button = new JRadioButton("16 pt.");
+		size16Button.addActionListener(listener);
+		size16Button.setSelected(true);
+		
+		size24Button = new JRadioButton("24 pt.");
+		size24Button.addActionListener(listener);
+		
+		size32Button = new JRadioButton("32 pt.");
+		size32Button.addActionListener(listener);
+		
+		size40Button = new JRadioButton("40 pt.");
+		size40Button.addActionListener(listener);
+		
+		ButtonGroup group = new ButtonGroup();
+		group.add(size8Button);
+		group.add(size16Button);
+		group.add(size24Button);
+		group.add(size32Button);
+		group.add(size40Button);
+		
+		JPanel panel = new JPanel();
+		panel.add(size8Button);
+		panel.add(size16Button);
+		panel.add(size24Button);
+		panel.add(size32Button);
+		panel.add(size40Button);
+		
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "Size"));
+
+		return panel;
+	}
+
+	/**
+	 * Gets user choice for font name, style, and size and sets the font of the text
+	 * sample.
+	 */
+	public void setLabelFont() {
+		// Get font name
+		String facename = (String) facenameCombo.getSelectedItem();
+		// Get font style
+
+		int style = 0;
+		if (italicCheckBox.isSelected()) {
+			style = style + Font.ITALIC;
+		}
+		if (boldCheckBox.isSelected()) {
+			style = style + Font.BOLD;
+		}
+
+		// Get font size
+
+		int size = 0;
+
+
+
+		if (size8Button.isSelected()) {
+			size = 8;
+		} else if (size16Button.isSelected()) {
+			size = 16;
+		} else if (size24Button.isSelected()) {
+			size = 24;
+		} else if (size32Button.isSelected()) {
+			size = 32;
+		} else if (size40Button.isSelected()) {
+			size = 40;
+		}
+		
+		
+		textStack.push(new Font(facename, style, size));
+//		textStack.push(new doClass(facenameCombo.getSelectedIndex(),
+//				italicCheckBox.isSelected(),
+//				boldCheckBox.isSelected(),
+//				size8Button.isSelected(),
+//				size16Button.isSelected(),
+//				size24Button.isSelected(),
+//				size32Button.isSelected(),
+//				size40Button.isSelected()
+//				));
+		
+		// Set font of text field
+		textArea.setFont(new Font(facename, style, size));
+		textArea.repaint();
+//		label.setFont(new Font(facename, style, size));
+//		label.repaint();
+	}
+	
+	
+//	class doClass {
+//		public int cI;
+//		public boolean iT;
+//		public boolean b;
+//		public boolean size8;
+//		public boolean size16;
+//		public boolean size24;
+//		public boolean size32;
+//		public boolean size40;
+//		doClass (int comboIndex, Boolean italic, Boolean bold, Boolean s8, Boolean s16, Boolean s24, Boolean s32, Boolean s40){
+//			cI = comboIndex;
+//			iT = italic;
+//			b = bold;
+//			size8 = s8;
+//			size16 = s16;
+//			size24 = s24;
+//			size32 = s32;
+//			size40 = s40;
+//			
+//		}
+//	}
+	
+//	public void doHelper(int comboIndex, Boolean italic, Boolean bold, Boolean s8, Boolean s16, Boolean s24, Boolean s32, Boolean s40) {
+//		facenameCombo.setSelectedIndex(comboIndex);
+//		italicCheckBox.setSelected(italic);
+//		boldCheckBox.setSelected(bold);
+//		size8Button.setSelected(s8);
+//		size16Button.setSelected(s16);
+//		size24Button.setSelected(s24);
+//		size32Button.setSelected(s32);
+//		size40Button.setSelected(s40);
+//		
+////		String facename = (String) facenameCombo.getSelectedItem();
+////		int style = 0;
+////		if (italicCheckBox.isSelected()) {
+////			style = style + Font.ITALIC;
+////		}
+////		if (boldCheckBox.isSelected()) {
+////			style = style + Font.BOLD;
+////		}
+////		int size = 0;
+////		if (size8Button.isSelected()) {
+////			size = 8;
+////		} else if (size16Button.isSelected()) {
+////			size = 16;
+////		} else if (size24Button.isSelected()) {
+////			size = 24;
+////		} else if (size32Button.isSelected()) {
+////			size = 32;
+////		} else if (size40Button.isSelected()) {
+////			size = 40;
+////		}
+////		
+////		// Set font of text field
+////		textArea.setFont(new Font(facename, style, size));
+////		textArea.repaint();
+//	}
+	
+	
+	public void undoEvent() {
+		
+//		System.out.println(textStack);
+//		textStack.pop();
+//		redoStack.push(textStack.pop());
+//		doHelper(textStack.lastElement().cI, 
+//				textStack.lastElement().iT, 
+//				textStack.lastElement().b, 
+//				textStack.lastElement().size8, 
+//				textStack.lastElement().size16, 
+//				textStack.lastElement().size24, 
+//				textStack.lastElement().size32, 
+//				textStack.lastElement().size40);
+		
+		redoStack.push(textStack.pop());
+		textArea.setFont(textStack.lastElement());
+		textArea.repaint();
+		
+	}
+	
+	public void redoEvent() {
+		textArea.setFont(redoStack.lastElement());
+		textArea.repaint();
+		textStack.push(redoStack.pop());
+		
+//		doClass dc = redoStack.lastElement();
+//		doHelper(dc.cI, dc.iT, dc.b, dc.size8, dc.size16, dc.size24, dc.size32, dc.size40);
+//		textStack.push(redoStack.pop());
+	}
+	
+	
+	public static void main(String[] args){
+		TextEditor te = new TextEditor();
+		te.setVisible(true);
+	   }
+}
